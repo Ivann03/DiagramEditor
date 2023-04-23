a40d6bf43f5f6dd9a7c11935e1e063dde59c178c
Ivann03 <sentiv@DESKTOP-1DF5IGF.localdomain>
2023-04-23 23:55:58 +0700

    Add file

---
+++ DiagramEditor/Models/AbstractElement.cs
@@ -0,0 +1,24 @@
+﻿using DiagramEditor.Models.Grids;
+using DiagramEditor.Models.Lines;
+using DynamicData.Binding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DiagramEditor.Models
+{
+    public abstract class AbstractElement : AbstractNotifyPropertyChanged
+    {
+        protected uint id;
+        private static uint id_generator = 0;
+        public AbstractElement()
+        {
+            id = id_generator++;
+        }
+
+        public uint ID { get => id; set => SetAndRaise(ref id, value); }
+    }
+}
